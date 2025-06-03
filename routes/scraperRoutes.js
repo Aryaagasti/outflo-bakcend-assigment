555d93c25fe13c516aa698ffa9e12a4a360c3266
Arya agasti <aryaagasti@gmail.com>
2025-06-03 19:13:42 +0530

    updating route proper

--- routes/scraperRoutes.js
+++ routes/scraperRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { scrapeLinkedInProfiles } = require('../services/linkedInScraper');
+const { scrapeLinkedInProfiles } = require('../utils/linkedInScraper');
 
 // Route to trigger LinkedIn profile scraping
 router.post('/scrape', async (req, res) => {
